fff755402510217c99b6d14278b6dc07d277e089
neelesh7singh <neeleshsingh767@gmail.com>
2020-06-15 00:46:09 +0530

    Updates

---
+++ precache-manifest.99a8bd59141cba4beed55540ec0e7677.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f572b6c6d5cc93e63f8da257819ee3d",
+    "url": "/react-animation-1/index.html"
+  },
+  {
+    "revision": "9f3bd2a0e5d627efed94",
+    "url": "/react-animation-1/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "eb9fd9d6355a81465ce3",
+    "url": "/react-animation-1/static/css/main.192d2bbd.chunk.css"
+  },
+  {
+    "revision": "9f3bd2a0e5d627efed94",
+    "url": "/react-animation-1/static/js/2.9f2e41fb.chunk.js"
+  },
+  {
+    "revision": "d7c9bccb0c21d951514f088172434d93",
+    "url": "/react-animation-1/static/js/2.9f2e41fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb9fd9d6355a81465ce3",
+    "url": "/react-animation-1/static/js/main.789524db.chunk.js"
+  },
+  {
+    "revision": "06e56be67fb281674caa",
+    "url": "/react-animation-1/static/js/runtime-main.442edbc2.js"
+  },
+  {
+    "revision": "4cd3b2b446ec7fe241401deca3057332",
+    "url": "/react-animation-1/static/media/arrow-left.4cd3b2b4.svg"
+  },
+  {
+    "revision": "116a70ada1a49d26fac6b51742f49e34",
+    "url": "/react-animation-1/static/media/arrow-right.116a70ad.svg"
+  },
+  {
+    "revision": "31be4c0b8ca8d104f9920d6b2a914cc4",
+    "url": "/react-animation-1/static/media/arrow-right.31be4c0b.svg"
+  },
+  {
+    "revision": "39fed6119aadb6126668ec3dc721848d",
+    "url": "/react-animation-1/static/media/boy.39fed611.webp"
+  },
+  {
+    "revision": "ab255543fcb9d5f490d60dff9db71677",
+    "url": "/react-animation-1/static/media/girl.ab255543.webp"
+  },
+  {
+    "revision": "00fcc588f8471e3562d62edf67c83daa",
+    "url": "/react-animation-1/static/media/image.00fcc588.jpg"
+  },
+  {
+    "revision": "b500dfd61c58377c0c72caae642e783a",
+    "url": "/react-animation-1/static/media/image2.b500dfd6.jpg"
+  },
+  {
+    "revision": "6dfebea3407a5de927f0aef8e126a401",
+    "url": "/react-animation-1/static/media/image3.6dfebea3.jpg"
+  },
+  {
+    "revision": "537a06540cea45a428fd905875650dc9",
+    "url": "/react-animation-1/static/media/mpumelelo-macu-l_YNobbDYJk-unsplash-min.537a0654.jpg"
+  }
+]);
